dad87f564952e637a83434f83f7bfc61c7689971
Julian Veit <claymore1298@gmail.com>
2025-06-17 09:03:16 +0200

    Updater: hide changelog menu item
    Change-Id: I8f3d432e491f5019cb276f45a3129823d64e8832

--- app/src/main/java/org/lineageos/updater/UpdatesActivity.java
+++ app/src/main/java/org/lineageos/updater/UpdatesActivity.java
@@ -284,11 +284,11 @@ public class UpdatesActivity extends UpdatesListActivity implements UpdateImport
         } else if (itemId == R.id.menu_preferences) {
             showPreferencesDialog();
             return true;
-        } else if (itemId == R.id.menu_show_changelog) {
+        /*} else if (itemId == R.id.menu_show_changelog) {
             Intent openUrl = new Intent(Intent.ACTION_VIEW,
                     Uri.parse(Utils.getChangelogURL(this)));
             startActivity(openUrl);
-            return true;
+            return true;*/
         } else if (itemId == R.id.menu_local_update) {
             mUpdateImporter.openImportPicker();
             return true;
